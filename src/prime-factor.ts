50d4b8e83a6dd5da21d51ca555b761daa5849317
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2025-01-16 22:03:49 +0530

    feat: implemented calculation for input 2

--- src/prime-factor.ts
+++ src/prime-factor.ts
@@ -1,6 +1,9 @@
 export class PrimeFactor {
-    public calculateFactors(input?: number): number[] {
+    public calculateFactors(input: number): number[] {
         const num = input;
+        if (num > 1) {
+            return [2];
+        }
         return [];
     }
 }
