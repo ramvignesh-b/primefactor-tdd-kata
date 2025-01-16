1060860d430cc6f2ed5f685bb806c53d8b64920d
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2025-01-16 23:48:13 +0530

    refactor: dynamically check for the prime divisors

--- src/prime-factor.ts
+++ src/prime-factor.ts
@@ -7,22 +7,15 @@ export class PrimeFactor {
         return this.findFactors(input);
     }
 
-    private findFactors(num: number): number[] {
+    private findFactors(input: number): number[] {
         const factors: number[] = [];
+        let num = input;
 
-        while (num % 2 === 0) {
-            factors.push(2);
-            num /= 2;
-        }
-
-        while (num % 3 === 0) {
-            factors.push(3);
-            num /= 3;
-        }
-
-        while (num % 5 === 0) {
-            factors.push(5)
-            num /= 5;
+        for (let divisor = 2; divisor <= num; divisor++) {
+            while (num % divisor === 0) {
+                factors.push(divisor);
+                num /= divisor;
+            }
         }
 
         if (num > 1) {
