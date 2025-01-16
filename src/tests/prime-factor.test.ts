af2d20275f1b20032b10c4a281740564df5c40b9
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2025-01-16 22:10:21 +0530

    test: add test for input 3

--- src/tests/prime-factor.test.ts
+++ src/tests/prime-factor.test.ts
@@ -14,4 +14,8 @@ describe("Prime Factor Calulator", () => {
     it("should return a list of factor [2] for input 2", () => {
         expect(primeFactor.calculateFactors(2)).toStrictEqual([2]);
     });
+
+    it('should return [3] for input 3', () => {
+        expect(primeFactor.calculateFactors(3)).toStrictEqual([3]);
+    });
 });
