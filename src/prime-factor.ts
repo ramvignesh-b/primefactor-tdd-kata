a2819425c87c29ababe80a884365a66d53d9eb0f
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2025-01-16 22:27:04 +0530

    feat: implemented loop to determine the factors of 2

--- src/prime-factor.ts
+++ src/prime-factor.ts
@@ -1,9 +1,21 @@
 export class PrimeFactor {
     public calculateFactors(input: number): number[] {
-        const num = input;
+        let num = input;
+        const factors: number[] = [];
+
         if (num <= 1) {
-            return [];
+            return factors;
         }
-        return [num];
+
+        while (num % 2 === 0) {
+            factors.push(2);
+            num /= 2;
+        }
+
+        if (input % 3 === 0) {
+            factors.push(3)
+        }
+
+        return factors;
     }
 }
