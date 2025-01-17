d7c24f2a355e37c61d2eb769e70c5a7de31e4e06
ramvignesh-b <43937419+ramvignesh-b@users.noreply.github.com>
2025-01-17 09:32:10 +0530

    feat: implemented prime factor logging

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import { PrimeFactor } from "./prime-factor";
 
+export const primeFactorCalculator = () => {
+    const input = 60;
+    const primeFactor = new PrimeFactor();
+    const factors = primeFactor.calculateFactors(input);
+
+    console.log(`Prime Factors of ${input}: ${factors}`);
+}
+
+primeFactorCalculator();
